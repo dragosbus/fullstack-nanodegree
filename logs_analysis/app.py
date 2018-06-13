642348b8a3bdddfa8e7a3c498be4d69829593310
dragosbus <dragosbusuioc94@gmail.com>
2018-06-13 09:21:25 +0200

    popular authors are getting correctly

--- logs_analysis/app.py
+++ logs_analysis/app.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+
 fname = open('result.txt','w')
 
 def connect_to_db(q):
@@ -27,10 +28,21 @@ def get_popular_articles():
     return res
 
 def popular_author():
-    '''Get most popilar authors'''
+    '''Get most popular authors'''
+
+    q = '''SELECT authors.name, SUM(article_views.views) AS total_views FROM authors JOIN article_views ON authors.id = article_views.author GROUP BY authors.id ORDER BY total_views DESC
+    '''
+    res = connect_to_db(q)
+
+    return res
 
 
 popular_articles = get_popular_articles()
 fname.write('1. What are the most popular three articles of all time?\n')
 for article in popular_articles:
     fname.writelines('{}-{}\n'.format(article[0], article[1]))
+
+popular_authors = popular_author()
+fname.writelines('\n2.Who are the most popular article authors of all time?')
+for author in popular_authors:
+    fname.writelines('{}-{}\n'.format(author[0], author[1]))
