6c206d66cc6414c8b6243d516304e6d874366eb2
dragosbus <dragosbusuioc94@gmail.com>
2018-07-04 11:05:57 +0200

    flask:setup database usign sqlalchemy

--- lectures/flask/app.py
+++ lectures/flask/app.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/hello')
 def root():
-    return 'Hello'
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '<br>'
+    return output
 
 
 if __name__ == '__main__':
